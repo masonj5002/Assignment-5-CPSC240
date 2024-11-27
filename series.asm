2b84db9125f35034e484510795f45003d056134d
Mason Jennings <masonj@csu.fullerton.edu>
2024-11-26 23:20:15 -0800

    11-26

--- series.asm
+++ series.asm
@@ -6,87 +6,110 @@ extern taylor
 extern printf
 extern scanf
 max_float_size equ 128
+max_int_size equ 128
 
 segment .data
-    inputmessage db "Please enter a float number value for x: ", 0
-    sampleoutput db "YOU ENTERED: %f", 10, 0 ;DELETE ME
-    time1 db "The time on the clock is now _______ tics and taylor has been called.", 10, 0
-    thankyoumsg db "Thank you for waiting", 10, 0
-    stringformat db "%s", 0
-    floatformat db "%lf", 0
+input_x_message db "Please enter a float number value for x: ", 0
+input_n_message db "Please enter the number of terms to include in the Taylor sum: ", 0
+thankyoumsg db "Thank you for waiting", 10, 0
+computedoutput db "The computed approximation of e^x is %lf with precision of 8 digits right of the point.", 10, 0
+goodbyemsg db "Thank you for using the Eyere expopnentional calculator", 10, 0
+goodbyemsgtwo db "Please return another day. The computed value will be set to caller functions.", 10, 0
+stringformat db "%s", 0
+floatformat db "%lf", 0
+intformat db "%d", 0
 
 segment .bss
-    float_value resq max_float_size
+float_value resq max_float_size
+int_value resq max_int_size
 
 segment .text
     ;****Program begins executing here****
 series:     ;****
-    ;Back up registers
-    ;PROLOG Backup GPRs (15 pushes)
-    push rbp
-    mov rbp, rsp
-    push rbx
-    push rcx
-    push rdx
-    push rdi
-    push rsi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-    pushf
-    ;GPRs are now in the stack
-    ;flow begins here:
-    ;input message
-    mov rax, 0
-    mov rdi, stringformat
-    mov rsi, inputmessage
-    call printf
+;Back up registers
+;PROLOG Backup GPRs (15 pushes)
+push rbp
+mov rbp, rsp
+push rbx
+push rcx
+push rdx
+push rdi
+push rsi
+push r8
+push r9
+push r10
+push r11
+push r12
+push r13
+push r14
+push r15
+pushf
+;GPRs are now in the stack
+;flow begins here:
+;obtain 'x'
+mov rax, 0
+mov rdi, stringformat
+mov rsi, input_x_message
+call printf
 
-    ;obtain floating-point value
-    mov qword rax, 0
-    mov rdi, floatformat
-    mov rsi, float_value
-    call scanf
-    movsd xmm0, [float_value]
-        
-    ;RECALLING FLOATING pt. VALUES:
-    mov rax, 1
-    mov rdi, sampleoutput
-    call printf
+mov qword rax, 0
+mov rdi, floatformat
+mov rsi, float_value
+call scanf
+movsd xmm0, [float_value]
+movsd xmm9, xmm0
 
-    mov rax, 0
-    call taylor
+;obtain 'n'
+mov rax, 0
+mov rdi, stringformat
+mov rsi, input_n_message
+call printf
 
-    ;"the time on the clock is now 1234567 tics and taylor has been called"
+mov qword rax, 0
+mov rdi, intformat
+mov rsi, int_value
+call scanf
 
-    mov rax, 0
-    mov rdi, stringformat
-    mov rsi, thankyoumsg
-    call printf
+mov rax, 0
+mov rdi, [int_value]
+call taylor
+movsd xmm15, xmm0
 
+mov rax, 0
+mov rdi, stringformat
+mov rsi, thankyoumsg
+call printf
 
-    ;flow ends here:
-    ;restore GPRs to their original state
-    popf
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsi
-    pop rdi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rbp
-    ; system stack has returned to initial state
+movsd xmm0, xmm15
+mov rax, 1
+mov rdi, computedoutput
+call printf
+
+mov rax, 0
+mov rdi, goodbyemsg
+call printf
+
+mov rax, 0
+mov rdi, goodbyemsgtwo
+call printf
+
+;flow ends here:
+;restore GPRs to their original state
+popf
+pop r15
+pop r14
+pop r13
+pop r12
+pop r11
+pop r10
+pop r9
+pop r8
+pop rsi
+pop rdi
+pop rdx
+pop rcx
+pop rbx
+pop rbp
+; system stack has returned to initial state
 ret
 ;** End of program **
--- taylor.asm
+++ taylor.asm
@@ -1,59 +1,163 @@
-; Mason Jennings
-; masonj@csu.fullerton.edu
+;Mason Jennings
+;masonj@csu.fullerton.edu
+;11/26/2024
 
 global taylor
 extern printf ;deleteme
+max_float_size equ 128
 
 segment .data
-    samplemsg db "Hello from taylor!", 10, 0 ;deleteme
-    stringformat db "%s", 0
+samplemsg db "Hello from taylor!", 10, 0 ;deleteme DELETE ALL MESSAGES
+samplefeedback db "The x value is: %f, the n value is %d", 10, 0 ;delemt
+sampletop db "Hello from TOP!", 10, 0 ;deleteme DELETE ALL MESSAGES
+float_valuen db "FLOAT VALUE TOP IS: %lf", 10, 0
+float_valueb db "FLOAT VALUE BOTTOM IS: %lf", 10, 0
+finalsoultion db "YOUR FINAL SOLUTION IS: %lf", 10, 0
+regvalue db "%d", 10, 0
+stringformat db "%s", 0
+value_zero dq 0.0
+value_one dq 1.0
 
 segment .bss
-    ;empty for now
+top resq max_float_size
+bottom resq max_float_size
+;r12 is our 'n' value
+
 segment .text
-    ;****Program begins executing here****
+;****Program begins executing here****
 taylor:     ;****
-    ;Back up registers
-    ;PROLOG Backup GPRs (15 pushes)
-    push rbp
-    mov rbp, rsp
-    push rbx
-    push rcx
-    push rdx
-    push rdi
-    push rsi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-    pushf
-    ;GPRs are now in the stack
-    ;flow begins here:
+;Back up registers
+;PROLOG Backup GPRs (15 pushes)
+push rbp
+mov rbp, rsp
+push rbx
+push rcx
+push rdx
+push rdi
+push rsi
+push r8
+push r9
+push r10
+push r11
+push r12
+push r13
+push r14
+push r15
+pushf
+;GPRs are now in the stack
+;flow begins here:
+;'x' value in xmm9
+mov r12, rdi     ;'n' value
+
+mov rax, 1
+mov rdi, samplefeedback
+movsd xmm0, xmm9
+mov rsi, r12
+call printf
+
+;FUNCATION BEGINS HERE:
+;create a loop that cycles n times
+;r12 is 'n'
+xor r13, r13 ;iterator
+movsd xmm15, [value_zero]   ;result
+summation_loop:
     mov rax, 0
     mov rdi, stringformat
     mov rsi, samplemsg
     call printf
-    ;flow ends here:
-    ;restore GPRs to their original state
-    popf
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsi
-    pop rdi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rbp
+
+    ;Creating the numerator
+    xor r14, r14 ;iterator
+    movsd xmm10, [value_one]  ;top value
+    top_loop:
+        cmp r14, 0      ;for x^0
+        je if_zero
+
+        mulsd xmm10, xmm9 ;top_value *= x;
+
+        top_resume:
+        inc r14
+        cmp r14, r13
+        jg top_end
+        jmp top_loop
+    top_end:
+
+    jmp if_zero_end
+    if_zero:
+        movsd xmm10, [value_one]
+        jmp top_resume
+    if_zero_end:
+
+    ;TESTING
+    movsd xmm0, xmm10         ; printf expects the float in xmm0
+    mov rax, 1               ; Clear rax (no variadic arguments), 0
+    mov rdi, float_valuen    ; Address of format string ("%lf\n")
+    call printf              ; Call printf
+
+    ;Creating the denominator
+    movsd xmm11, [value_one]    ;result of bottom
+    movsd xmm12, [value_one]    ;float counter (skipping zero)
+    mov r14, 1                  ;iterator (skipping zero)
+    ;TODO: Create if statement for 0! (which = 1)
+    cmp r13, 0
+    jne bottom_loop
+    movsd xmm11, [value_one]
+    jmp bottom_end
+
+    bottom_loop: ;(ex: 4!)
+        mulsd xmm11, xmm12
+
+        addsd xmm12, [value_one]
+        inc r14
+        cmp r14, r13
+        jg bottom_end
+        jmp bottom_loop
+    bottom_end:
+
+    ;TESTING
+    movsd xmm0, xmm11         ; printf expects the float in xmm0
+    mov rax, 1               ; Clear rax (no variadic arguments), 0
+    mov rdi, float_valueb    ; Address of format string ("%lf\n")
+    call printf              ; Call printf
+
+    movsd xmm14, xmm10 
+    divsd xmm14, xmm11 ;top / bottom
+    addsd xmm15, xmm14; += xmm14
+
+    ;TESTING
+    movsd xmm0, xmm15         ; printf expects the float in xmm0
+    mov rax, 1               ; Clear rax (no variadic arguments), 0
+    mov rdi, finalsoultion    ; Address of format string ("%lf\n")
+    call printf              ; Call printf
+
+    inc r13
+    cmp r13, r12
+    jg summation_end
+    jmp summation_loop
+summation_end:
+;TESTING
+movsd xmm0, xmm15         ; printf expects the float in xmm0
+mov rax, 1               ; Clear rax (no variadic arguments), 0
+mov rdi, finalsoultion    ; Address of format string ("%lf\n")
+call printf              ; Call printf
+movsd xmm0, xmm15
+;flow ends here:
+;restore GPRs to their original state
+popf
+pop r15
+pop r14
+pop r13
+pop r12
+pop r11
+pop r10
+pop r9
+pop r8
+pop rsi
+pop rdi
+pop rdx
+pop rcx
+pop rbx
+pop rbp
     ; system stack has returned to initial state
 ret
 ;** End of program **
